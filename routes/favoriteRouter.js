8a81f27c56b2f0c65ce6c5cc594a85d8b0582421
Makhambet <makhambet.torezhan@nu.edu.kz>
2020-07-21 22:31:41 +0500

    add showPretty endpoint

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -31,6 +31,43 @@ favoriteRouter.get('/', authenticate.verifyUser, (req, res, next) => {
     .catch(err => console.log(err));
 });
 
+favoriteRouter.get('/showPretty', authenticate.verifyUser, (req, res, next) => {
+    Favorites.findOne({ user: req.user._id }, (err, favorite) => {
+        if(err) console.log(err);
+
+        if(!favorite) {
+            res.statusCode = 403;
+            res.end("No favorites found!!");
+        }
+    })
+    .populate('user')
+    .populate('films')
+    .then(favorites => {
+        
+        var titles = [];
+        var years = [];
+        var genres = [];
+        var posters = [];
+
+        for(var i = 0; i < favorites.films.length; i++) {
+            titles.push(favorites.films[i].title + ';');
+            years.push(favorites.films[i].year);
+            genres.push(favorites.films[i].genre + ';');
+            posters.push(favorites.films[i].poster);
+        }
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.render('favorites.show.hbs', {
+            pageTitle: 'Favorites Films of ' + req.user.username,
+            titles,
+            years,
+            genres,
+            posters
+        });
+    })
+    .catch(err => console.log(err));
+});
 
 favoriteRouter.get('/add', authenticate.verifyUser, (req, res, next) => {
 
@@ -40,17 +77,7 @@ favoriteRouter.get('/add', authenticate.verifyUser, (req, res, next) => {
     });
 });
 
-favoriteRouter.get('/:filmId', authenticate.verifyUser, (req, res, next) => {
-    Favorites.findById(req.params.filmId)
-    .populate('user')
-    .populate('films')
-    .then(favorites => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(favorites);
-    })
-    .catch(err => console.log(err));
-});
+
 
 
 favoriteRouter.post('/submit', authenticate.verifyUser, (req, res, next) => {
